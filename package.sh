783ec8d3cfa4970e7f269cd8d2f98f26e7b696bb
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-06-04 11:01:28 +0200

    use debsigs

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 rm -f mypackage-latest.deb
 dpkg-deb --build mypackage mypackage-latest.deb
-gpg --default-key E78912536665F4C59CD0742EC7D287C8C1CAC373 --detach-sign mypackage-latest.deb
+debsigs --sign=origin -k E78912536665F4C59CD0742EC7D287C8C1CAC373 mypackage-latest.deb
